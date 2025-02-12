2f89f91ba8932cc76345d7e01c9ccbe5010e6700
alfredoperry14 <alfredoperry14@gmail.com>
2025-02-12 13:35:20 -0800

    Initial Commit

---
+++ JobMarketApp/JobMarketAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  JobMarketAppApp.swift
+//  JobMarketApp
+//
+//  Created by Alfredo Perry on 2/12/25.
+//
+
+import SwiftUI
+
+@main
+struct JobMarketAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
